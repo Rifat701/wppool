745b30134e9d4190a76aeb2fcbcc6a28e5420349
Md. Rifat Reza <014rjakash@gmail.com>
2024-07-10 16:17:32 +0600

    update

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "b-200": "rgb(239, 243, 246)",
+        "b-300": "#eff3f6",
+        "b-400": "#191618",
+        "d-100": "#647888",
+        "r-100": "#FC714D",
+        "g-100": "#7CA63A",
+        "p-100": "#6F34A1",
+        "w-100": "#748DA1",
+        blue: "#2042b6",
+      },
+    },
+  },
+  plugins: [],
+};
